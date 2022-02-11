2b5632d69b896c59608af08e76a64c59cfa73a8d
UnnatiTecocraft <99247348+UnnatiTecocraft@users.noreply.github.com>
2022-02-11 16:04:10 +0530

    Made changes in format

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
-import todo from '../Images/todologo.png';
+import React, { useState } from "react";
+import todo from "../images/todoLogo.png";
 
 const Todo = () => {
 
-    const [input, setInput] = useState("");
+    const [input, setInput] = useState('');
     const [list, setList] = useState([]);
     const [toggle, setToggle] = useState(true);
-    const [isEdit, setIsEdit] = useState(null);
+    const [editIndex, setEditIndex] = useState(null);
 
     const addItem = () => {
-        if(!input){
-            alert('Fill data...');
-        }else if(input && !toggle){
+        if (!input) {
+            alert("Fill data...");
+        } else if (input && !toggle){
             setList(
                 list.map((ele) => {
-                    if( ele.id == isEdit){
-                        return {...list,name: input};
+                    if (ele.id == editIndex){
+                        return {...list, name: input};
                     }
                     return ele;
                 })
             );
             setToggle(true);
-            setInput(""); 
-            setIsEdit(null);
-        }else{
+            setInput(''); 
+            setEditIndex(null);
+        } else {
             const allItems = { id: new Date().getTime().toString(), name: input};
             setList([...list, allItems]);
-            setInput("");
+            setInput('');
         }
     };
 
@@ -36,7 +36,7 @@ const Todo = () => {
         });
         setToggle(false);
         setInput(editedData.name); 
-        setIsEdit(index);  
+        setEditIndex(index);  
     }
 
     const deleteItem = (index) => {
@@ -50,46 +50,44 @@ const Todo = () => {
         setList([]);
     }
 
-  return (
-    <>
-        <div className='main-div'>
-            <div className='child-div'>
-                <figure>
-                    <img src={todo} alt="Image not found" />
-                    <figcaption>Add your list here 📋</figcaption>
-                </figure>
-                <div className='addItems'>
-                    <input type="text" placeholder='✍️   Add Items...' value={input} onChange={(e) => setInput(e.target.value)}/>
-                    {
-                    
-                        toggle ?  <i className='fa fa-plus add-btn' title='Add Item' onClick={addItem}></i> : 
-                                <i className='fa fa-edit add-btn' title='Add Item' onClick={addItem}></i>                       
-                    }
-                </div>
-                <div className='showItems'>
-                    {
-                        list.map((val) => {
-                            return (
-                                <div className='eachItem' key={val.id}>
-                                    <h3>{val.name}</h3>
-                                    <div className='todo-btn'>
-                                        <i className='far fa-edit add-btn' title='Edit Item' onClick={() => editItem(val.id)}></i>
-                                        <i className='far fa-trash-alt add-btn' title='Delete Item' onClick={() => deleteItem(val.id)}></i>
+    return (
+        <>
+            <div className='main-div'>
+                <div className='child-div'>
+                    <figure>
+                        <img src={todo} alt="Image not found" />
+                        <figcaption>Add your list here 📋</figcaption>
+                    </figure>
+                    <div className='addItems'>
+                        <input type="text" placeholder='✍️   Add Items...' value={input} onChange={(e) => setInput(e.target.value)}/>
+                        {
+                            toggle ?  <i className='fa fa-plus add-btn' title='Add Item' onClick={addItem}></i> : 
+                                    <i className='fa fa-edit add-btn' title='Add Item' onClick={addItem}></i>                       
+                        }
+                    </div>
+                    <div className='showItems'>
+                        {
+                            list.map((val) => {
+                                return (
+                                    <div className='eachItem' key={val.id}>
+                                        <h3>{val.name}</h3>
+                                        <div className='todo-btn'>
+                                            <i className='far fa-edit add-btn' title='Edit Item' onClick={() => editItem(val.id)}></i>
+                                            <i className='far fa-trash-alt add-btn' title='Delete Item' onClick={() => deleteItem(val.id)}></i>
+                                        </div>
                                     </div>
-                                </div>
-                            );
-                        })
-                    }
-                </div>
-                
-                <div className='showItems'>
-                    <button className='btn effect04' data-sm-link-text="Remove All" onClick={deleteAll}><span>Check List</span></button>
+                                );
+                            })
+                        }
+                    </div>
+                    <div className='showItems'>
+                        <button className='btn effect04' data-sm-link-text="Remove All" onClick={deleteAll}><span>Check List</span></button>
+                    </div>
                 </div>
             </div>
-        </div>
 
-    </>
-  )
+        </>
+    )
 }
 
 export default Todo;
